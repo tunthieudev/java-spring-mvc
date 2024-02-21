c1291b9da412ac4c25a0cab5bac2568a9ed46efb
Thiều Ngọc Tuấn <thieutuanptit@gmail.com>
2024-02-21 23:35:34 +0700

    spring security chapter 6

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hoi Dan IT!";
+        return "Hello World update";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access this page";
     }
 
 }
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,18 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+
+		// container
+		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : hoidanit.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
